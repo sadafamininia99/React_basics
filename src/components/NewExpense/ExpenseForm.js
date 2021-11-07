fb1c1f0a8a7d5f7b1e58c62546a25e06fb2d6036
sadafamininia99 <aminicomputer96@gmail.com>
2021-11-07 17:55:43 +0330

    done

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,7 +1,8 @@
-import React from 'react';
+
+  
+import React, { useState } from 'react';
 
 import './ExpenseForm.css';
-
 
 const ExpenseForm = () => {
   const [enteredTitle, setEnteredTitle] = useState('');
@@ -19,32 +20,31 @@ const ExpenseForm = () => {
   const dateChangeHandler = (event) => {
     setEnteredDate(event.target.value);
   };
-  //we can have multiple useSte per cmp
 
   return (
     <form>
       <div className='new-expense__controls'>
         <div className='new-expense__control'>
           <label>Title</label>
-          <input type='text' onChange={titleChangeHandler}/>
+          <input type='text' onChange={titleChangeHandler} />
         </div>
         <div className='new-expense__control'>
           <label>Amount</label>
-          <input 
-          type='number' 
-          min='0.01'
-           step='0.01' 
-           onChange={amountChangeHandler} 
-           />
+          <input
+            type='number'
+            min='0.01'
+            step='0.01'
+            onChange={amountChangeHandler}
+          />
         </div>
         <div className='new-expense__control'>
           <label>Date</label>
-          <input 
-          type='date' 
-          min='2019-01-01'
-           max='2022-12-31' 
-           onChange={dateChangeHandler} 
-           />
+          <input
+            type='date'
+            min='2019-01-01'
+            max='2022-12-31'
+            onChange={dateChangeHandler}
+          />
         </div>
       </div>
       <div className='new-expense__actions'>
