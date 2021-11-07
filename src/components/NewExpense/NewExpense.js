87f5b851bf7e8b305effb281115dc4368ae232f6
sadafamininia99 <aminicomputer96@gmail.com>
2021-11-07 12:01:25 +0330

    all dependecy in this file added

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import ExpenseForm from './ExpenseForm';
 import './NewExpense.css';
 
 
 function NewExpense() {
   return <div className="new-expense">
-     
+     <ExpenseForm />
       </div>
 };
 
