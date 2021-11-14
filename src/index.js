b727bf1f2c45bf8cb2a2c4a23357638c28286756
sadafamininia99 <aminicomputer96@gmail.com>
2021-11-14 20:09:21 +0330

    All done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-import reactDom from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 import './index.css';
 import App from './App';
 
-reactDom.render(<App/>,document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
