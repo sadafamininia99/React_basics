be540b5ffff07cbb3c2d6fbdb6e3a9f2458ba0a9
sadafamininia99 <aminicomputer96@gmail.com>
2021-11-17 19:16:25 +0330

    back to last date and time  :/

--- src/components/Expenses/ExpenseDate.js
+++ src/components/Expenses/ExpenseDate.js
@@ -1,25 +1,17 @@
 import React from 'react';
 
-/**
- * yarn add jalali-moment
- * OR
- * npm install jalali-moment -S
- */
-import moment from 'jalali-moment';
-
 import './ExpenseDate.css';
 
 const ExpenseDate = (props) => {
-//   const month = props.date.toLocaleString('en-US', { month: 'long' });
-//   const day = props.date.toLocaleString('en-US', { day: '2-digit' });
-//   const year = props.date.getFullYear();
+  const month = props.date.toLocaleString('en-US', { month: 'long' });
+  const day = props.date.toLocaleString('en-US', { day: '2-digit' });
+  const year = props.date.getFullYear();
 
   return (
     <div className='expense-date'>
-      {moment(props.date).format('jYYYY/jMM/jDD')}
-//       <div className='expense-date__month'>{month}</div>
-//       <div className='expense-date__year'>{year}</div>
-//       <div className='expense-date__day'>{day}</div>
+      <div className='expense-date__month'>{month}</div>
+      <div className='expense-date__year'>{year}</div>
+      <div className='expense-date__day'>{day}</div>
     </div>
   );
 };
